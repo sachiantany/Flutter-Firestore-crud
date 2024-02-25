ad919277db62bff17dbb2bfa9e4568ce34085de2
sachiantany <sachi.antany@gmail.com>
2024-02-25 17:30:50 +0530

    first commit

---
+++ lib/services/firestore.dart
@@ -0,0 +1,13 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreServices {
+  //get all data
+  // final CollectionReference data;
+  //create data
+
+  //read data
+
+  //update data
+
+  //delete data
+}
